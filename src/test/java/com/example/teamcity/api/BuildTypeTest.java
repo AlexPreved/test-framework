d50ff5a807fa7925a7e75317a948aa9b8ab6e7df
sunny_macbook <t89137899903@gmail.com>
2025-02-20 00:00:53 +0300

    added tests(templates) with allure steps

---
+++ src/test/java/com/example/teamcity/api/BuildTypeTest.java
@@ -0,0 +1,51 @@
+package com.example.teamcity.api;
+
+import org.testng.annotations.Test;
+
+import static io.qameta.allure.Allure.step;
+
+@Test(groups = {"Regression"})
+public class BuildTypeTest {
+    @Test(description = "User should be able to create build type", groups = {"Positive", "CRUD"})
+    public void testUserCreatesBuildType_Returns000_WhenTypeNotExists() {
+        step("Create user");
+        step("Create project by user");
+        step("Create buildType in project by user");
+        step("Check buildtype was created successfully");
+
+    }
+
+    @Test(description = "User should not be able to create build type with not unique id", groups = {"Negative", "CRUD"})
+    public void testUserCreatesBuildType_Returns000_WhenTypeIdNotUnique() {
+        step("Create user");
+        step("Create project by user");
+        step("Create buildType1 in project by user");
+        step("Create buildType2 in project by user with the same id");
+        step("Check buildtype2 was not created with statusCode 401");
+
+    }
+
+    @Test(description = "Project admin should be able to create build type", groups = {"Positive", "Roles"})
+    public void testProjectAdminCreatesBuildType_Returns000_WhenTypeNotExists() {
+        step("Create user");
+        step("Create project by user");
+        step("Grant user PROJECT_ADMIN role in project");
+        step("Create buildType in project by user(PROJECT_ADMIN)");
+        step("Check buildtype was created successfully");
+
+    }
+
+    @Test(description = "Project admin should not be able to create build type for another project", groups = {"Negative", "Roles"})
+    public void testProjectAdminCreatesBuildType_Returns000_ForNotTheirProject() {
+        step("Create user1");
+        step("Create project1 by user1");
+        step("Grant user1 PROJECT_ADMIN role in project1");
+
+        step("Create user2");
+        step("Create project2 by user2");
+
+        step("Create buildType in project2 by user1(PROJECT_ADMIN)");
+        step("Check buildtype was not created with statusCode 403");
+
+    }
+}
